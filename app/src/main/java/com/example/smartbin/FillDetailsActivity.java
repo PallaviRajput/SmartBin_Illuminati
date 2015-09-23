44cc2f06a27c432b9c43b4ae544a8c4b06ce0272
PallaviRajput <pallavi.rajput83@gmail.com>
2015-09-24 04:29:53 +0530

    Smart bin Dashboard dummy FillActivity,LocationActivity

---
+++ app/src/main/java/com/example/smartbin/FillDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartbin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FillDetailsActivity extends Activity{
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fill_details_layout);
+    }
+}
---
+++ app/src/main/java/com/example/smartbin/LocationActivity.java
@@ -0,0 +1,13 @@
+package com.example.smartbin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LocationActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_layout);
+    }
+}
